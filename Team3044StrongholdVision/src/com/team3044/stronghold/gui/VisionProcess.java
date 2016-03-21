860e68ce341ff6fcb83291936f0e6d7a7ce81fd9
Joey <joeylyon1@gmail.com>
2016-03-20 21:53:07 -0400

    Added More to config and connect to robot

--- Team3044StrongholdVision/src/com/team3044/stronghold/gui/VisionProcess.java
+++ Team3044StrongholdVision/src/com/team3044/stronghold/gui/VisionProcess.java
@@ -35,9 +35,13 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
+import com.team3044.stronghold.vision.Main;
+
+import edu.wpi.first.smartdashboard.robot.Robot;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import jssc.SerialPortException;
 
-public class VisionProcess implements KeyListener,MouseListener {
+public class VisionProcess implements KeyListener, MouseListener {
 
 	private int state = 6;
 	final int INIT = 6;
@@ -51,212 +55,11 @@ public class VisionProcess implements KeyListener,MouseListener {
 	VideoCapture camera = new VideoCapture();
 	Mat cameraFrame = new Mat();
 	int count = 0;
-	
+
 	int mouseX = 0;
 	int mouseY = 0;
 	int oldX = 0;
 	int oldY = 0;
-	
-	public void process() {
-		switch (state) {
-		case INIT:
-			if(camera.isOpened())
-				camera.release();
-			if (isAxis) {
-				if (openCamera(output)) {
-					output.print("Camera Opened");
-					this.state = MAIN_LOOP;
-				} else {
-
-				}
-			} else {
-				this.camera = new VideoCapture(0);
-				System.out.println("Using Laptop Camera");
-				if (camera.isOpened())
-					this.state = MAIN_LOOP;
-			}
-			break;
-		case CONNECT_CAMERA:
-			break;
-		case CONNECT_ROBOT:
-			break;
-		case CONNECT_SERIAL:
-			break;
-		case MAIN_LOOP:{
-
-		System.out.println("-------- Start:" + (start = System.currentTimeMillis()) + "---------");
-		
-		camera.read(frame);
-		if (frame.size().width > 0) {
-			count += 1;
-			frame.copyTo(orig);
-
-			Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
-			Core.inRange(frame, new Scalar(H_MIN, S_MIN, V_MIN), new Scalar(H_MAX, S_MAX, V_MAX), threshold);
-
-			threshold.copyTo(tmp2);
-
-			Imgproc.erode(threshold, threshold, erodeElement);
-			
-			Imgproc.dilate(threshold, threshold, dilateElement);
-			
-			
-
-			ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-			Imgproc.findContours(threshold, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE,
-					new Point(0, 0));
-
-			boundingRects = new ArrayList<Rect>();
-
-			for (int i = 0; i < contours.size(); i++) {
-				
-				contour = contours.get(i);
-				MatOfPoint2f thisContour2f = new MatOfPoint2f();
-				MatOfPoint approxContour = new MatOfPoint();
-				MatOfPoint2f approxContour2f = new MatOfPoint2f();
-				contour.convertTo(thisContour2f, CvType.CV_32FC2);
-				Imgproc.approxPolyDP(thisContour2f, approxContour2f, 2, true);
-				approxContour2f.convertTo(approxContour, CvType.CV_32S);
-				if (approxContour.size().height < 100 && approxContour.size().height > 0) {
-					
-					System.out.println(approxContour.get(0, 0)[0]);
-					Rect r = Imgproc.boundingRect(contour);
-					contours.set(i, approxContour);
-					
-					boundingRects.add(r);
-					Imgproc.drawContours(orig, contours, i, new Scalar(0, 0, 255));
-				}else{
-					contours.set(i, new MatOfPoint());
-				}
-				
-			}
-
-			biggestRectid = 0;
-			ArrayList<Rect> boundingRect2 = new ArrayList<Rect>();
-			Rect largeBadRectangle = new Rect();
-			for (int i = 0; i < boundingRects.size(); i++) {
-
-				Rect r = boundingRects.get(i);
-				if (r.area() > 7000) {
-					largeBadRectangle = r;
-				}
-				if (r.area() > 500 || r.width / r.height > 2) {
-					// System.out.println(r.area());
-					Scalar mean = Core.mean(orig.submat(r));
-					Mat centerSeventyFive = new Mat();
-					Rect midSeventyFive = new Rect((int) (r.tl().x) + (int) (r.width * .25),
-							(int) (r.tl().y)/* + (int)(r.height * .25) */, (int) (r.width * .75),
-							(int) (r.height * .75));
-					centerSeventyFive = tmp2.submat(midSeventyFive);
-				
-					for (int j = midSeventyFive.x; j < midSeventyFive.x + midSeventyFive.size().width; j++) {
-						for (int k = midSeventyFive.y; k < midSeventyFive.y + midSeventyFive.size().height; k++) {
-							
-							if (tmp2.get(k, j)[0] == 255) {
-								tmp2.put(k, j, 0);
-								
-							} else {
-								tmp2.put(k, j, 255);
-							
-							}
-						}
-					}
-
-					if (Core.sumElems(tmp2.submat(midSeventyFive)).val[0] / r.area() > 100
-							&& Core.sumElems(tmp2.submat(midSeventyFive)).val[0] / r.area() < 200) {
-
-						if (r.area() > 7000) {
-
-						} else {
-							if (!largeBadRectangle.contains(r.tl())) {
-								boundingRect2.add(r);
-								Imgproc.rectangle(orig, r.tl(), r.br(), new Scalar(0, 255, 0));
-							}
-						}
-
-					}
-
-				}
-
-			}
-			int maxId = 0;
-			for (int i = 0; i < boundingRect2.size(); i++) {
-				Rect r = boundingRect2.get(i);
-				if (r.size().area() > boundingRect2.get(maxId).size().area()
-						&& (r.size().height / r.size().width < 1)) {
-					maxId = i;
-				} else if ((r.size().height / r.size().width < 1) && r.size().area() > 1000) {
-					maxId = i;
-				}
-			}
-			if (boundingRect2.size() > 0) {
-				Imgproc.rectangle(orig, boundingRect2.get(maxId).tl(), boundingRect2.get(maxId).br(),
-						new Scalar(255, 0, 0), 1);
-				Imgcodecs.imwrite("C:\\opencv3.0.0\\" + String.valueOf(count) + ".jpg", orig);
-				//sendRectangles(boundingRect2, maxId, visionTable, orig);
-			}
-
-			// Imgproc.cvtColor(threshold, threshold,
-			// Imgproc.COLOR_BGR2HSV);
-			Mat finalMat = new Mat();
-			orig.copyTo(finalMat);
-			Imgproc.resize(finalMat, finalMat, new Size(640, 480));
-			mainImage.pushImage(finalMat);
-			mainImage.repaint();
-
-			System.out.println("--------End: " + (System.currentTimeMillis() - start) + "---------");
-			if (System.currentTimeMillis() - start < 30) {
-				try {
-					Thread.sleep((long) (30 - (System.currentTimeMillis() - start)));
-				} catch (InterruptedException e) {
-					output.println(e.getMessage());
-				}
-			}
-		}
-		}
-			break;
-		
-		case CALIBRATE:
-			Imgproc.cvtColor(this.frame, this.orig, Imgproc.COLOR_BGR2HSV);
-			this.mainImage.pushImage(orig);
-			if(this.clickCount != oldClickCount){
-				
-				this.H_MIN = (int) orig.get(mouseX, mouseY)[0];
-				this.S_MIN = (int) orig.get(mouseX, mouseY)[1];
-				this.V_MIN = (int) orig.get(mouseX, mouseY)[2];
-				this.H_MAX = (int) orig.get(mouseX, mouseY)[0];
-				this.S_MAX = (int) orig.get(mouseX, mouseY)[1];
-				this.V_MAX = (int) orig.get(mouseX, mouseY)[2];
-				System.out.println(H_MIN);
-				this.oldClickCount = 0;
-				System.out.println(new Scalar(H_MIN,S_MIN,V_MIN));
-				clickCount = 0;
-			}
-			
-			Core.inRange(orig, new Scalar(H_MIN, S_MIN, V_MIN), new Scalar(H_MAX, S_MAX, V_MAX), threshold);
-			this.thresholdWindow.pushImage(threshold);
-			if(thresholdWindow.isVisible() == false){
-				BufferedWriter writer;
-				try {
-					writer = Files.newBufferedWriter(
-							Paths.get(System.getenv("APPDATA") + "\\3044Vision\\config.txt"), StandardOpenOption.WRITE);
-					writer.write(H_MIN + "\n" + S_MIN + "\n" + V_MIN + "\n" + H_MAX + "\n" + S_MAX + "\n" + V_MAX + "\n"
-							+ "http://10.30.44.20/axis-cgi/mjpg/video.cgi?test.mjpeg" + "\n" + "COM5\n" + offset + "/nAXIS");
-
-					writer.flush();
-					writer.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-				this.state = MAIN_LOOP;
-			}
-			break;
-		case DEBUG:
-			break;
-		}
-	}
 
 	int H_MIN = 0, S_MIN = 0, V_MIN = 0;
 	int H_MAX = 180, S_MAX = 255, V_MAX = 255;
@@ -264,14 +67,14 @@ public class VisionProcess implements KeyListener,MouseListener {
 
 	private Path configSaveLocation = Paths.get(System.getenv("APPDATA") + "\\3044Vision\\config.txt");
 
-	ImageWindow thresholdWindow = new ImageWindow("DEBUG", 640, 480, this,false);
-	ImageWindow mainImage = new ImageWindow("CameraImage", 640, 480, this,true);
+	ImageWindow thresholdWindow = new ImageWindow("DEBUG", 640, 480, this, false);
+	ImageWindow mainImage = new ImageWindow("CameraImage", 640, 480, this, true);
 	OptionSelector selector = new OptionSelector("Begin", 300, 200, this);
-	ConsoleWindow output = new ConsoleWindow("Console", 500, 500, this,false);
+	ConsoleWindow output = new ConsoleWindow("Console", 500, 500, this, false);
 
 	private String cameraAddr = "http://10.30.44.20/axis-cgi/mjpg/video.cgi?test.mjpeg";
 	private String serialPort = "COM5";
-	
+
 	Mat frame = new Mat();
 	Mat blank = new Mat();
 	Mat threshold = new Mat();
@@ -282,7 +85,6 @@ public class VisionProcess implements KeyListener,MouseListener {
 	Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
 	Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
 
-	
 	MatOfPoint contour;
 	int biggestRectid = 0;
 
@@ -295,6 +97,8 @@ public class VisionProcess implements KeyListener,MouseListener {
 	boolean isAxis = true;
 	private int clickCount = 0;
 	private int oldClickCount = 0;
+
+	NetworkTable visionTable;
 
 	public VisionProcess() {
 		thresholdWindow.setVisible(false);
@@ -322,6 +126,222 @@ public class VisionProcess implements KeyListener,MouseListener {
 			return false;
 		}
 
+	}
+
+	public void process() {
+		switch (state) {
+		case INIT:
+			frame = new Mat();
+			blank = new Mat();
+			threshold = new Mat();
+			temp = new Mat();
+			orig = new Mat();
+			load = Mat.zeros(new Size(320, 240), CvType.CV_8UC3);
+			hierarchy = new Mat();
+			erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+			dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+			state = CONNECT_CAMERA;
+			break;
+		case CONNECT_CAMERA:
+			if (camera.isOpened())
+				camera.release();
+			if (isAxis) {
+				if (openCamera(output)) {
+					output.print("Camera Opened");
+					this.state = MAIN_LOOP;
+				} else {
+
+				}
+			} else {
+				this.camera = new VideoCapture(0);
+				System.out.println("Using Laptop Camera");
+				if (camera.isOpened())
+					this.state = MAIN_LOOP;
+			}
+			break;
+
+		case CONNECT_ROBOT:
+			this.connectToRobot("roboRIO-3044-frc.local");
+			this.state = MAIN_LOOP;
+			break;
+		case CONNECT_SERIAL:
+			break;
+		case DEBUG:
+			thresholdWindow.setVisible(true);
+			thresholdWindow.pushImage(threshold);
+			thresholdWindow.repaint();
+
+		case MAIN_LOOP: {
+
+			System.out.println("-------- Start:" + (start = System.currentTimeMillis()) + "---------");
+
+			camera.read(frame);
+			if (frame.size().width > 0) {
+				count += 1;
+				frame.copyTo(orig);
+
+				Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
+				Core.inRange(frame, new Scalar(H_MIN, S_MIN, V_MIN), new Scalar(H_MAX, S_MAX, V_MAX), threshold);
+
+				threshold.copyTo(tmp2);
+
+				Imgproc.erode(threshold, threshold, erodeElement);
+
+				Imgproc.dilate(threshold, threshold, dilateElement);
+
+				ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+				Imgproc.findContours(threshold, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE,
+						new Point(0, 0));
+
+				boundingRects = new ArrayList<Rect>();
+
+				for (int i = 0; i < contours.size(); i++) {
+
+					contour = contours.get(i);
+					MatOfPoint2f thisContour2f = new MatOfPoint2f();
+					MatOfPoint approxContour = new MatOfPoint();
+					MatOfPoint2f approxContour2f = new MatOfPoint2f();
+					contour.convertTo(thisContour2f, CvType.CV_32FC2);
+					Imgproc.approxPolyDP(thisContour2f, approxContour2f, 2, true);
+					approxContour2f.convertTo(approxContour, CvType.CV_32S);
+					if (approxContour.size().height < 100 && approxContour.size().height > 0) {
+
+						System.out.println(approxContour.get(0, 0)[0]);
+						Rect r = Imgproc.boundingRect(contour);
+						contours.set(i, approxContour);
+
+						boundingRects.add(r);
+						Imgproc.drawContours(orig, contours, i, new Scalar(0, 0, 255));
+					} else {
+						contours.set(i, new MatOfPoint());
+					}
+
+				}
+
+				biggestRectid = 0;
+				ArrayList<Rect> boundingRect2 = new ArrayList<Rect>();
+				Rect largeBadRectangle = new Rect();
+				for (int i = 0; i < boundingRects.size(); i++) {
+
+					Rect r = boundingRects.get(i);
+					if (r.area() > 7000) {
+						largeBadRectangle = r;
+					}
+					if (r.area() > 500 || r.width / r.height > 2) {
+
+						Mat centerSeventyFive = new Mat();
+						Rect midSeventyFive = new Rect((int) (r.tl().x) + (int) (r.width * .25),
+								(int) (r.tl().y)/* + (int)(r.height * .25) */, (int) (r.width * .75),
+								(int) (r.height * .75));
+						centerSeventyFive = tmp2.submat(midSeventyFive);
+
+						for (int j = midSeventyFive.x; j < midSeventyFive.x + midSeventyFive.size().width; j++) {
+							for (int k = midSeventyFive.y; k < midSeventyFive.y + midSeventyFive.size().height; k++) {
+
+								if (tmp2.get(k, j)[0] == 255) {
+									tmp2.put(k, j, 0);
+
+								} else {
+									tmp2.put(k, j, 255);
+
+								}
+							}
+						}
+
+						if (Core.sumElems(tmp2.submat(midSeventyFive)).val[0] / r.area() > 100
+								&& Core.sumElems(tmp2.submat(midSeventyFive)).val[0] / r.area() < 200) {
+
+							if (r.area() > 7000) {
+
+							} else {
+								if (!largeBadRectangle.contains(r.tl())) {
+									boundingRect2.add(r);
+									Imgproc.rectangle(orig, r.tl(), r.br(), new Scalar(0, 255, 0));
+								}
+							}
+
+						}
+
+					}
+
+				}
+				int maxId = 0;
+				for (int i = 0; i < boundingRect2.size(); i++) {
+					Rect r = boundingRect2.get(i);
+					if (r.size().area() > boundingRect2.get(maxId).size().area()
+							&& (r.size().height / r.size().width < 1)) {
+						maxId = i;
+					} else if ((r.size().height / r.size().width < 1) && r.size().area() > 1000) {
+						maxId = i;
+					}
+				}
+				if (boundingRect2.size() > 0) {
+					Imgproc.rectangle(orig, boundingRect2.get(maxId).tl(), boundingRect2.get(maxId).br(),
+							new Scalar(255, 0, 0), 1);
+					Imgcodecs.imwrite("C:\\opencv3.0.0\\" + String.valueOf(count) + ".jpg", frame);
+					Main.sendRectangles(boundingRect2, maxId, visionTable, orig);
+				}
+
+				// Imgproc.cvtColor(threshold, threshold,
+				// Imgproc.COLOR_BGR2HSV);
+				Mat finalMat = new Mat();
+				orig.copyTo(finalMat);
+				Imgproc.resize(finalMat, finalMat, new Size(640, 480));
+				mainImage.pushImage(finalMat);
+				mainImage.repaint();
+
+				System.out.println("--------End: " + (System.currentTimeMillis() - start) + "---------");
+				if (System.currentTimeMillis() - start < 30) {
+					try {
+						Thread.sleep((long) (30 - (System.currentTimeMillis() - start)));
+					} catch (InterruptedException e) {
+						output.println(e.getMessage());
+					}
+				}
+			}
+		}
+			break;
+
+		case CALIBRATE:
+			Imgproc.cvtColor(this.frame, this.orig, Imgproc.COLOR_BGR2HSV);
+			this.mainImage.pushImage(orig);
+			if (this.clickCount != oldClickCount) {
+
+				this.H_MIN = (int) orig.get(mouseX, mouseY)[0];
+				this.S_MIN = (int) orig.get(mouseX, mouseY)[1];
+				this.V_MIN = (int) orig.get(mouseX, mouseY)[2];
+				this.H_MAX = (int) orig.get(mouseX, mouseY)[0];
+				this.S_MAX = (int) orig.get(mouseX, mouseY)[1];
+				this.V_MAX = (int) orig.get(mouseX, mouseY)[2];
+				System.out.println(H_MIN);
+				this.oldClickCount = 0;
+				System.out.println(new Scalar(H_MIN, S_MIN, V_MIN));
+				clickCount = 0;
+			}
+
+			Core.inRange(orig, new Scalar(H_MIN, S_MIN, V_MIN), new Scalar(H_MAX, S_MAX, V_MAX), threshold);
+			this.thresholdWindow.pushImage(threshold);
+			if (thresholdWindow.isVisible() == false) {
+				BufferedWriter writer;
+				try {
+					writer = Files.newBufferedWriter(Paths.get(System.getenv("APPDATA") + "\\3044Vision\\config.txt"),
+							StandardOpenOption.WRITE);
+					writer.write(H_MIN + "\n" + S_MIN + "\n" + V_MIN + "\n" + H_MAX + "\n" + S_MAX + "\n" + V_MAX + "\n"
+							+ "http://10.30.44.20/axis-cgi/mjpg/video.cgi?test.mjpeg" + "\n" + "COM5\n" + offset
+							+ "/nAXIS");
+
+					writer.flush();
+					writer.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				this.state = MAIN_LOOP;
+			}
+			break;
+
+		}
 	}
 
 	public void restart() {
@@ -362,12 +382,11 @@ public class VisionProcess implements KeyListener,MouseListener {
 
 	}
 
-	public void connectToCamera(String address) {
-
-	}
-
 	public void connectToRobot(String host) {
-
+		Robot.setHost(host);
+		Robot.setUseMDNS(true);
+		Robot.setTeam(3044);
+		visionTable = NetworkTable.getTable("SmartDashboard");
 	}
 
 	public void initializeSerialConnection(String port) {
@@ -386,7 +405,7 @@ public class VisionProcess implements KeyListener,MouseListener {
 
 	public void onAxis() {
 		this.state = INIT;
-		
+
 		BufferedWriter writer;
 		try {
 			writer = Files.newBufferedWriter(Paths.get(System.getenv("APPDATA") + "\\3044Vision\\config.txt"),
@@ -429,18 +448,22 @@ public class VisionProcess implements KeyListener,MouseListener {
 		this.oldY = mouseY;
 		thresholdWindow.setVisible(true);
 		state = CALIBRATE;
-		
+
 		System.out.println("calib");
 	}
 
 	public void onDebug() {
+		if (state == DEBUG) {
+			this.state = MAIN_LOOP;
+		} else {
+			state = DEBUG;
+		}
 		System.out.println("debug");
 	}
 
 	public void onReconnect() {
 		this.state = INIT;
 
-		
 		System.out.println("reconnect");
 	}
 
@@ -559,32 +582,28 @@ public class VisionProcess implements KeyListener,MouseListener {
 		this.mouseY = mouse.getY();
 		this.clickCount = mouse.getClickCount();
 		this.oldClickCount = -1;
-		//System.out.println(clickCount);
-		
+		// System.out.println(clickCount);
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		
-		
+
 	}
 
 }
